06b3139fcd99bbc01bfd13b38dd9548904f8d820
Michael Pearson <michaelpearsun@gmail.com>
2020-12-11 14:08:20 -0600

    End of Week 3

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -25,6 +25,8 @@ function RenderCampsite(props) {
 
     const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
 
+    const recognizeComment = ({dx}) => (dx > 200) ? true : false;
+
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderGrant: () => {
@@ -51,6 +53,8 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if (recognizeComment(gestureState)) {
+                props.onShowModal();
             }
             return true;
         }
